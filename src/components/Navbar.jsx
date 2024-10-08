488e27a870720d51441e011c3f305f511a07932e
Patrick Ferreira <patrickpierreff@gmail.com>
2024-10-08 18:01:09 -0300

    about and blocking routes in the NavBar if logged or not

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,30 +2,75 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import styles from './Navbar.module.css' 
 
+import { useAuthentication } from '../hooks/useAuthentication'
+
+import { useAuthValue } from '../context/AuthContext'
+
 const Navbar = () => {
+  const { user } = useAuthValue()
+
   return (
     <nav className={styles.navbar}>
-      <NavLink to="/" className={styles.brand}>
+      <NavLink 
+        to="/" 
+        className={styles.brand}
+      >
         Mini <span>Blog</span>
       </NavLink>
       <ul className={styles.links_list}>
         <li>
-          <NavLink to="/" className={({isActive}) => (isActive ? styles.active : "")}>
+          <NavLink 
+            to="/" 
+            className={({isActive}) => (isActive ? styles.active : "")}
+          >
             Home
           </NavLink>
         </li>
+        {!user && (
+          <>
+            <li>
+              <NavLink 
+                to="/login" 
+                className={({isActive}) => (isActive ? styles.active : "")}
+              >
+                Login
+              </NavLink>
+            </li>
+            <li>
+              <NavLink 
+                to="/register" 
+                className={({isActive}) => (isActive ? styles.active : "")}
+              >
+                Register
+              </NavLink>
+            </li>
+          </>
+        )}
+        {user && (
+          <>
+            <li>
+              <NavLink 
+                to="/posts/create" 
+                className={({isActive}) => (isActive ? styles.active : "")}
+              >
+                New Post
+              </NavLink>
+            </li>
+            <li>
+              <NavLink 
+                to="/dashboard" 
+                className={({isActive}) => (isActive ? styles.active : "")}
+              >
+                Dashboard
+              </NavLink>
+            </li>
+          </>
+        )}
         <li>
-          <NavLink to="/login" className={({isActive}) => (isActive ? styles.active : "")}>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/register" className={({isActive}) => (isActive ? styles.active : "")}>
-            Register
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/about" className={({isActive}) => (isActive ? styles.active : "")}>
+          <NavLink 
+            to="/about" 
+            className={({isActive}) => (isActive ? styles.active : "")}
+          >
             About
           </NavLink>
         </li>
