9f22d7bf8a8dbda6e1aa4fcda0aa987d1de19de4
Patrick Ferreira <patrickpierreff@gmail.com>
2024-10-16 15:35:36 -0300

    hook to fetch documents from firebase

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate, Link } from 'react-router-dom'
 
 // CSS
 import styles from "./Home.module.css"
+import { useFetchDocuments } from '../../hooks/useFetchDocuments'
+
+
+// components
 
 const Home = () => {
+  const [query, setQuery] = useState("")
+  const {documents: posts, loading, error} = useFetchDocuments("posts")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.home}>
+      <h1>See our recent posts...</h1>
+      <form onSubmit={handleSubmit} className={styles.search_form}>
+        <input 
+          type="text" 
+          placeholder='... or search for tags.' 
+          onChange={(e) => setQuery(e.target.value)} />
+        <button className="btn btn-dark">Search</button>
+      </form>
+
+      <div>
+        {!loading && <p>Loading...</p>}
+        {posts && posts.map((post) => {
+          <h3>{post.title}</h3>
+        })}
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Posts not found</p>
+            <Link to="/posts/create" className='btn'>
+              Be the first to post!
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
