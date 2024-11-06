7930de67997f2fc1c3c3ae3d5c3bd3785739e52d
Patrick Ferreira <patrickpierreff@gmail.com>
2024-11-06 18:35:26 -0300

    dashboard created

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,58 @@
 import React from 'react'
 import styles from "./Dashboard.module.css"
+import { Link } from 'react-router-dom'
+
+// hooks
+import { useAuthValue } from "../../context/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments"
 
 const Dashboard = () => {
+  const {user} = useAuthValue()
+  const uid = user.uid
+
+  // User posts
+  const {documents: posts, loading} = useFetchDocuments("posts", null, uid)
+
+  const deleteDocument = () => {}
+
+  if(loading) {
+    return <p>Loading...</p>
+  }
+
   return (
-    <div>
-        <h1>Dashboard</h1>
+    <div className={styles.dashboard}>
+        <h2>Dashboard</h2>
+        <p>Manage your posts.</p>
+        {posts && posts.length === 0 ? (
+          <div className={styles.noposts}>
+            <p>No posts found</p>
+            <Link to="/posts/create" className='btn'>Create first post</Link>
+          </div>
+        ) : (
+          <>
+            <div className={styles.post_header}>
+              <span>Title</span>
+              <span>Actions</span>
+            </div>
+
+            {posts && posts.map((post) => (
+              <div key={post.id} className={styles.post_row}>
+                <p>{post.title}</p>
+                <div>
+                  <Link to={`/posts/${post.id}`} className='btn btn-outline'>
+                    View
+                  </Link>
+                  <Link to={`/posts/edit/${post.id}`}  className='btn btn-outline'>
+                    Edit
+                  </Link>
+                  <button onClick={() => deleteDocument(post.id)} className='btn btn-outline btn-danger'>
+                    Delete
+                  </button>
+                </div>
+              </div>
+            ))}
+          </>
+        )}
     </div>
   )
 }
